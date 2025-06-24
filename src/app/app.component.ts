64c93e43e3d8df6bf6251f90eea1919a77d7b4c4
peterson <petersonsimiao1@gmail.com>
2025-06-23 22:56:33 -0300

    Montando a tela inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "./shared/components/layout/footer/footer.component";
 import { HeaderComponent } from "./shared/components/layout/header/header.component";
-import { FaqComponent } from './shared/components/layout/faq/faq.component';
 import { HowWorksComponent } from './shared/components/layout/how-works/how-works.component';
 import { ResumeExampleComponent } from './shared/components/layout/resume-example/resume-example.component';
 import { LandingPageComponent } from "./pages/landing-page/landing-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [FooterComponent, HeaderComponent, FaqComponent, HowWorksComponent, ResumeExampleComponent, LandingPageComponent],
+  imports: [FooterComponent, HeaderComponent, HowWorksComponent, ResumeExampleComponent, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
